d05f42e5afb0bb587ed896dba1c293c31ba29397
Kirito <aftabemir123@gmail.com>
2024-07-17 13:12:30 +0530

    User Entity

---
+++ src/main/java/com/ot/moto/entity/User.java
@@ -0,0 +1,31 @@
+package com.ot.moto.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String phone;
+
+    private String password;
+
+    private String role;
+
+}
