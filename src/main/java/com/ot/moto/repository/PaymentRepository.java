9787ae5f34b25c8f5dcf71b5d7de420cbfca34c8
Ullas <ullas3651.uv@gmail.com>
2024-08-22 19:13:16 +0530

    update master added

--- src/main/java/com/ot/moto/repository/PaymentRepository.java
+++ src/main/java/com/ot/moto/repository/PaymentRepository.java
@@ -1,5 +1,6 @@
 package com.ot.moto.repository;
 
+import com.ot.moto.entity.Driver;
 import com.ot.moto.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -21,6 +22,6 @@ public interface PaymentRepository extends JpaRepository<Payment,Long> {
     @Query("SELECT SUM(p.amount) FROM Payment p WHERE p.date >= :startDate AND p.date <= :endDate")
     public Double sumAmountForCurrentMonth(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 
-    @Query("SELECT p FROM Payment p JOIN p.driver d WHERE LOWER(d.username) LIKE LOWER(CONCAT('%', :username, '%'))")
+    @Query("SELECT p FROM Payment p JOIN p.driver d WHERE LOWER(TRIM(d.username)) LIKE LOWER(CONCAT('%', :username, '%'))")
     public List<Payment> findPaymentsByDriverNameContaining(@Param("username") String username);
 }
