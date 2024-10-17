c8892c6032614fe5a2b5606913246baae34c1968
Ullas <ullas3651.uv@gmail.com>
2024-10-17 16:36:07 +0530

    metrics apis

--- src/main/java/com/ot/moto/repository/TamMetricRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ot.moto.repository;
-
-import com.ot.moto.entity.TamMetrics;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TamMetricRepository extends JpaRepository<TamMetrics,Long> {
-}
---
+++ src/main/java/com/ot/moto/repository/TamMetricsRepository.java
@@ -0,0 +1,15 @@
+package com.ot.moto.repository;
+
+import com.ot.moto.entity.TamMetrics;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+
+public interface TamMetricRepository extends JpaRepository<TamMetrics,Long> {
+
+    public Page<TamMetrics> findAllByDateTimeBetween(LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
+
+    public Page<TamMetrics> findAllByDateTimeBetween(Pageable pageable);
+}
